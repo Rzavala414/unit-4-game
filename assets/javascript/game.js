a70caf9f12b5ef87bff3b0e12f361b400157de2c
Roger Zavala <Rogeliozavala15@gmail.com>
2019-09-04 12:57:03 -0700

    added id 'crystal' to the crystal row, and got all crystals to add up to one number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,27 +7,34 @@ $("#playerGoal").text(playerGoal);
 //CREATE A RANDOM NUMBER FOR EACH GEM
 //creates a random number between 1-12 for the gems
 var valueOfBlueGem = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-var valueOfRedGem = Math.floor(Math.random() * (12 - 1 + 1) + 1);
 var valueOfGreenGem = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+var valueOfRedGem = Math.floor(Math.random() * (12 - 1 + 1) + 1);
 var valueOfYellowGem = Math.floor(Math.random() * (12 - 1 + 1) + 1);
 var userScore = 0;
-//Collect USER input when CRYSTAL is clicked
+
+console.log("Blue Gem: " + valueOfBlueGem);
+console.log("Green Gem: " + valueOfGreenGem);
+console.log("Red Gem: " + valueOfRedGem);
+console.log("Yellow Gem: " + valueOfYellowGem);
+
+//Collects USER input when CRYSTAL is clicked
+//HAVE THE NUMBER THE USER CLICKED ON ADD TO THE TOTAL SCORE BOX
 $("#valueOfBlueCrystal").on("click", function() {
-  //   alert("hey i work!!!");
-  //   userScore = valueOfBlueGem;
   userScore = userScore + valueOfBlueGem;
   console.log(userScore);
 });
-$("#valueOfRedCrystal").on("click", function() {
-  alert("hey i work!!!");
-});
 $("#valueOfGreenCrystal").on("click", function() {
-  alert("hey i work!!!");
+  userScore = userScore + valueOfGreenGem;
+  console.log(userScore);
+});
+$("#valueOfRedCrystal").on("click", function() {
+  userScore = userScore + valueOfRedGem;
+  console.log(userScore);
 });
 $("#valueOfYellowCrystal").on("click", function() {
-  alert("hey i work!!!");
+  userScore = userScore + valueOfYellowGem;
+  console.log(userScore);
 });
-//TODO:HAVE THE NUMBER THE USER CLICKED ON ADD TO THE TOTAL SCORE BOX
 //TODO:HAVE THE WINS GO UP IF NUMBER MATCHES
 //TODO:HAVE LOSES GO UP WHEN PLAYER GOES OVER GIVEN VALUE
 //TODO:HAVE THE GAME RESET WITH DIFFERENT VALUES FOR THE GEMS
